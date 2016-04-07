89b16c374934fb7f22a0f6746519f129880d4ddb
Thomas <tirril@wireless1x-155-41-103-88.bu.edu>
2016-04-07 17:07:41 -0400

    more xorigin stuff

--- hw5d2/server.js
+++ hw5d2/server.js
@@ -53,24 +53,14 @@ app.post('/dist', bodyParser.json(), function (req, res) {
 	
 });
 
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
+//CORS middleware
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'example.com');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+	next();
+}
+app.use(allowCrossDomain);
 
 app.listen(3000);
 console.log("Server running on port 3000");
