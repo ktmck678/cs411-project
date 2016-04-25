7a1843fa963f17d1211929868298378e1fca22e3
Thomas Hsu <tomdenotela@gmail.com>
2016-04-25 18:41:09 -0400

    Test endpoint.

---
+++ final_del/server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.post('/dist', bodyParser.json(), function (req, res, next) {
+	//check body
+	if !('timeSpan' in req.body) or !('calendars' in req.body) {
+		res.json({'error':'invalid or malformed request'})
+	}
+	res.json({'times':[
+		'datetime':'2016-04-26T17:00:00',
+		'dayOfWeek':'Tuesday'
+		]})
+});
+
+app.listen(3000);
+console.log("Server running on port 3000");
